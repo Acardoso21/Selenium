19dfdbe0a677a4be80db3a2940018f2cddebb423
Andre <acardoso.ny@gmail.com>
2024-01-22 10:04:03 +0000

    adding more controls to csv interface, and updating testing files

--- selenium_scripts/csv_interface.py
+++ selenium_scripts/csv_interface.py
@@ -12,7 +12,10 @@ class CsvWriter:
             writer.writerow([datetime.now().strftime('%Y-%m-%d %H:%M:%S')] + self.headers)
 
     def append_data(self, data):
+        if not isinstance(data, list):
+            data = [data]
+
         # append data to the CSV file
         with open(self.filename, 'a', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([datetime.now().strftime('%Y-%m-%d %H:%M:%S')] + data) 
+            writer.writerow([datetime.now().strftime('%Y-%m-%d %H:%M:%S')] + data)
